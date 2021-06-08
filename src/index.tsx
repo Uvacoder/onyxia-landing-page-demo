4d2bfa48b2d0b48bdfc1c1cf5c023e2ce33282b8
William Thiery <williamalexthiery@hotmail.fr>
2021-06-08 23:42:51 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import headerImg from "./assets/header/header-img-example.png";
 import type {GitlandingProps} from "gitlanding";
 
 
-const props: GitlandingProps = {
+export const props: GitlandingProps = {
     "header": {
 
         "image": {
@@ -47,7 +47,7 @@ sur le traitement statistique et la datascience.
     },
 
     "mainSection": {
-        "dataBlocks": [
+        "sections": [
             {
                 "article": {
                     "title": "Quelques mots à propos d’Onyxia",
@@ -80,6 +80,42 @@ Proin sed tellus eu turpis porttitor feugiat ac quis est. Nullam metus tellus, g
                             "title": "Actualités et projets"
                         }
                     ]
+                }
+            },
+            {
+                "title": "La collaboration au sein de la communauté",
+                "thumbNails": {
+                    "thumbNails": [
+                        {
+                            "title": "Le gitlab du datalab"
+                        },
+                        {
+                            "title": "Le salon d’échange Tchap"
+                        },
+                        {
+                            "title": "Les outils collaboratifs MIM-Libre"
+                        }
+                    ]
+
+                },
+                "article": {
+                    "title": "Comment contribuer à la communauté ?",
+                    "paragraphMd": `
+Nulla convallis ligula erat, eget suscipit erat luctus quis. Quisque nec augue sagittis, dignissim enim sit amet, gravida felis. Nulla et eleifend velit integer pulvinar nibh. 
+
+Proin sed tellus eu turpis porttitor feugiat ac quis est. Nullam metus tellus, gravida ut velit quis, accumsan commodo risus. Etiam nec metus id elit hendrerit suscipit molestie quis mauris. Aliquam quis nunc ac purus placerat rhoncus. Vestibulum semper quis leo tempus porta nulla suscipit ante sit.
+`,
+                    "button": {
+                        "title": "Contribuer",
+                        "href": ""
+                    },
+                    
+                },
+                "illustration": {
+                    "type": "image",
+                    "imageProps": {
+                        "url": headerImg
+                    }
                 }
             }
         ]
